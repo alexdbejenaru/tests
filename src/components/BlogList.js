d21beec1e52606e1f691afe9534a1e7ba3411c90
Alex Bejenaru <alex.d.bejenaru@gmail.com>
2021-09-27 22:28:18 +0300

    made progress on navbar and blog articles

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,7 +1,10 @@
 const BlogList = (props) => {
-    let blogs = props.blogs;
+    const blogs = props.blogs;
+    const title = props.title;
+
     return (
         <div className="blog-list">
+            <h2>{ title }</h2>
             {blogs.map((article, index) => (
                 <div className="blog-preview" key={index}>
                     <h3>{ article.title }</h3>
